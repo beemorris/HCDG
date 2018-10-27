2de2bb15c5463ae01c3a2f6a0fef8dde04e9235a
beemorris <morrisrebeccar@gmail.com>
2018-10-27 17:00:17 -0400

    added Cleaner.py

---
+++ Cleaner.py
@@ -0,0 +1,25 @@
+def main():
+	def getInputFile():
+		bad = True
+		while bad:
+			try:
+				fileName = input("Enter file name: ")
+				# Open file for input
+				f = open(fileName, "r")
+				bad = False
+			except Exception as err:
+				print("Please enter a valid file name:")
+
+	def clean():
+		file = getInputFile()
+		lines = file.readlines()
+		for line in lines:
+			line = line.rstrip()
+			line = line.replace('\n', '')
+			tokenized = [len(line.split())]  # tokenizes the sentence
+
+		print(tokenized)
+
+	clean()
+
+main()
